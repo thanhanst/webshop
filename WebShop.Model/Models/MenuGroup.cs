1c613725e902a4ab8de3b6b8f420c2052cdee9b8
DESKTOP-8N48GDC\thanhanst <thanhanst@gmail.com>
2018-02-24 11:18:57 +0700

    add Models

---
+++ WebShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int ID { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+        public virtual IEnumerable<Menu> Menus { set; get; }
+    }
+}
---
+++ WebShop.Model/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Model.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]        
+        public string Alias { set; get; }
+        public string Image { set; get; }
+        public string MoreImage { set; get; }
+        [Required]
+        public double Price { set; get; }
+        public double Promotion { set; get; }
+        public int Warranty { set; get; }
+        public string Description { set; get; }
+        public string Content { set; get; }
+        public string MetaKeyword { set; get; }
+        public string MetaDescription { set; get; }
+        [Required]
+        public DateTime CreateDate { set; get; }
+        [Required]
+        public string CreateBy { set; get; }
+        public DateTime UpdateDate { set; get; }
+        public string UpdateBy { set; get; }
+        public bool Status { set; get; }
+        public bool HomeFlag { set; get; }
+        public bool HotFlag { set; get; }
+        public int ViewCount { set; get; }
+    }
+}
